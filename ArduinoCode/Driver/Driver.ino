7c32519c430e852bd075312b75107a405428bbc8
Eric Wimberley <wimberleyeric@gmail.com>
2019-02-28 21:07:14 -0500

    Motor position init

--- ArduinoCode/Driver/Driver.ino
+++ ArduinoCode/Driver/Driver.ino
@@ -1,5 +1,8 @@
-int sensorPin1 = A0;    // select the input pin for the potentiometer
-int sensorPin2 = A1;    // select the input pin for the potentiometer
+#define MAIN_HALL_SENSOR A0
+#define SECONDARY_HALL_SENSOR A1
+
+#define MOTOR_LEFT 2
+#define MOTOR_RIGHT 3
 
 int time = 0;
 int lastTime = 0;
@@ -7,26 +10,49 @@ int state = 0;
 int ups = 0;
 int downs = 0;
 
+//mode 0 = seek to home position
+//mode 1 = sweep
+int mode = 0;
+
 void setup() {
   Serial.begin(9600);
+  pinMode(MOTOR_LEFT,OUTPUT);
+  pinMode(MOTOR_RIGHT,OUTPUT); 
+  digitalWrite(MOTOR_RIGHT, LOW);
+  digitalWrite(MOTOR_LEFT, LOW);
 }
 
 void loop() {
-  time = millis();
-  if((time - lastTime) > 1000){
-    lastTime = time;
-    int freq = (ups + downs) / 2;
-    Serial.println(freq);
-    ups = 0;
-    downs = 0;
-  }
-  int sensorValue1 = analogRead(sensorPin1);
-  if(sensorValue1 > 500 && state == 0){
-    ups = ups + 1;
-    state = 1;
-  }
-  if(sensorValue1 <= 500 && state == 1){
-    downs = downs + 1;
-    state = 0;
+  //delay(200); 
+  if(mode == 0){
+    int mainHall = analogRead(MAIN_HALL_SENSOR);
+    //Serial.println(mainHall);
+    if(mainHall > 20){
+      // turn the motor left
+      digitalWrite(MOTOR_LEFT, HIGH);
+    } else {
+      digitalWrite(MOTOR_LEFT, LOW);
+      mode = 1;
+    }
+  } else if(mode == 1){
+    int secondaryHall = analogRead(SECONDARY_HALL_SENSOR);
+    digitalWrite(MOTOR_RIGHT, HIGH);
+    if(secondaryHall > 500 && state == 0){
+      ups = ups + 1;
+      state = 1;
+    }
+    if(secondaryHall <= 500 && state == 1){
+      downs = downs + 1;
+      state = 0;
+    }
+
+    time = millis();
+    if((time - lastTime) > 1000){
+      lastTime = time;
+      int freq = (ups + downs) / 2;
+      Serial.println(freq);
+      ups = 0;
+      downs = 0;
+    }
   }
 }
